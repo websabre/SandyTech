0a118d03bf0e79570248e13aa9726a74010a399d
websabre <sandeep.kothapalli1@gmail.com>
2025-03-16 17:15:01 +0530

    Updated contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -17,13 +17,13 @@ const Contact = () => {
           <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-              <p className="mt-1 text-gray-700">Silicon Valley, CA, USA</p>
+              <p className="mt-1 text-gray-700">Hyderabad, India</p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-              <a className="text-gray-500 leading-relaxed">example@coderblog.com</a>
+              <a className="text-gray-500 leading-relaxed">sandeep.kothapalli1@gmail.com</a>
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-              <p className="leading-relaxed text-gray-700">123-456-7890</p>
+              <p className="leading-relaxed text-gray-700">+91-8019145771</p>
             </div>
           </div>
         </div>
